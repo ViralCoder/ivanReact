7d092dcce8eb6b0451f3c287e0477d7ab57896d1
khanin_daniil <khanin_daniil@mail.ru>
2017-04-20 21:26:41 +0300

    first Commit

---
+++ main.js
@@ -0,0 +1,6 @@
+/**
+ * Created by user on 20.04.2017.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const webpack = require('webpack');
+const config = {
+    entry: './main.js',
+    output: {
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015', 'react']
+                    }
+                }
+            }
+        ]
+    },
+    watch:true,
+    devtool:'source-map'
+}
+module.exports = config;
